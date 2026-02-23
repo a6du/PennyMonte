fb46c652b557a809cc1add4198715df6feff8a1c
a6du <abdullahbinthaha@gmail.com>
2026-02-23 13:37:33 +0530

    Add the create, update, version, isDeleted, operationType to category

--- src/main/java/com/a6du/PennyMonte/model/Category.java
+++ src/main/java/com/a6du/PennyMonte/model/Category.java
@@ -3,6 +3,15 @@ package com.a6du.PennyMonte.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -27,6 +36,27 @@ public class Category {
     @Enumerated(EnumType.STRING)
     @Column(name = "category_type")
     private CategoryType categoryType;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Version
+    private int version;
+
+    @Builder.Default
+    @JsonIgnore
+    private boolean isDeleted = false;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "operation_type")
+    private OperationType operationType;
+
+    public enum OperationType {
+        CREATE, UPDATE, DELETE
+    }
 
     public enum CategoryType {
         INCOME, EXPENSE
